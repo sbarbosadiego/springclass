de4f603c8e64d23031f3a84e1ec13800515e36dd
Diego Barbosa <diego.barbosa.pro@gmail.com>
2025-12-02 23:56:59 -0300

    Sobe

--- src/main/java/br/com/springclass/springclass/model/Curso.java
+++ src/main/java/br/com/springclass/springclass/model/Curso.java
@@ -1,4 +1,18 @@
 package br.com.springclass.springclass.model;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Curso {
+
+    private Long id;
+    private String curso;
+    private String descricao;
+
 }
